53bb1e0431119f561d65dfe919cf149bf0495e8a
Alex Zheng <zhengal3x@gmail.com>
2024-01-28 16:10:04 -0500

    added doubly linked list to track number of times a floor request was made on that floors and a floor request was made for a certain floor form the elevator

--- elevatorClass.cpp
+++ elevatorClass.cpp
@@ -6,8 +6,70 @@
 float averageElevatorPeople = 1.9;
 
 typedef float DataType;
+typedef int DataType2;
 //TODO add a list that tracks the number of times each floor was pressed from the inside and out of the elevator
 //NOW CREATE A LIST with the number of times each floor was pressed.
+
+struct Node1 {
+    DataType2 outside;
+    DataType2 inside;
+    Node1 *back;
+    Node1 *next;
+
+    Node1(): inside(0), outside(0), back(nullptr), next(nullptr) {};
+    Node1(int x, int y): outside(x), inside(y), back(nullptr), next(nullptr) {};
+};
+
+class FloorsList {
+private:
+    Node1 *head = nullptr;
+public:
+    void insert(int value1, int value2, int position) {
+        Node1 *newNode;
+        newNode = new Node1(value1, value2);
+        if(position == 0) {
+            newNode->next = head;
+            head = newNode;
+            return;
+        }
+        else {
+            Node1 *previous = head;
+            Node1 *forward = head->next;
+            while(position > 1) {
+                if(forward == nullptr)
+                    break;
+                previous = forward; //can do previous->next
+                forward = forward->next;
+                position--;
+            }
+            newNode->back = previous;
+            newNode->next = forward;
+            previous->next = newNode;
+            if(forward != nullptr)
+                forward->back = newNode;
+        }
+    }
+    void add(int value1, int value2, int position) {
+        Node1 *newNode = head;
+        while(position > 0) {
+            newNode = newNode->next;
+            position--;
+        }
+        newNode->outside += value1;
+        newNode->inside += value2;
+    }
+    void print() {
+        int floor = 1;
+        Node1 *newNode;
+        newNode = head;
+        while(newNode != nullptr) {
+            std::cout<<"The number of requests to leave floor "<<floor<<" was: "<< newNode->outside<<std::endl;
+            std::cout<<"The number of requests to go to floor "<<floor<<" from inside the elevator is: "<< newNode->inside<<std::endl;
+            newNode = newNode->next;
+            floor++;
+        }
+    }
+};
 
 struct Node {
     DataType data;
@@ -15,15 +77,6 @@ struct Node {
 
     Node(): data(0), next(nullptr) {};
     explicit Node(float x): data(x*averageElevatorPeople), next(nullptr) {};
-};
-
-class FloorsList {
-private:
-    Node *head = nullptr;
-public:
-    void insert (DataType value, int position) {
-
-    }
 };
 
 class PeopleList {
@@ -34,8 +87,8 @@ public:
         Node *newNode = new Node(value);
         if(position == 0) {
             newNode->next = head;
-            head = newNode; //fix this when there are things after.
-            return;
+            head = newNode;
+            return; //realistically the original head just gets shoved back.
         }
         else {
             Node *previous = head;
@@ -90,6 +143,15 @@ public:
             std::cout<<newNode->data<<std::endl;
             newNode = newNode->next;
         }
+    }
+    void add(float value, int position) {
+        Node *newNode;
+        newNode = head;
+        while(position > 0) {
+            newNode = newNode->next;
+            position--;
+        }
+        newNode->data += value;
     }
 };
 
@@ -146,9 +208,11 @@ int main () {
     Elevator elevator(floors);
 
     PeopleList peoplePerFloor;
+    FloorsList buttonsPressed;
 
     for(int i =0; i < floors; i++) {
         peoplePerFloor.insert(0, i);
+        buttonsPressed.insert(0,0, i);
     }
 
     int floorRequest;
@@ -156,11 +220,13 @@ int main () {
     std::cout<<"which floor do you want to go to?\n";
     std::cin>>floorRequest;
 
-    peoplePerFloor.replace(averageElevatorPeople, floorRequest-1);
+    buttonsPressed.add(1,0,0);
+    peoplePerFloor.add(averageElevatorPeople, floorRequest-1);
 
-    peoplePerFloor.remove(6);
+    //peoplePerFloor.remove(6);
 
     peoplePerFloor.print();
+    buttonsPressed.print();
 
     return 0;
 }
